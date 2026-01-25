a4bf54dc4e3872a4a5d378675591935af4175d27
Mendel Ashkenazi <mendelashkenazi@gmail.com>
2026-01-25 08:44:47 -0500

    Move umami script

---
+++ umami.js
@@ -0,0 +1,8 @@
+// Umami Analytics
+(function() {
+  var script = document.createElement('script');
+  script.defer = true;
+  script.src = 'https://cloud.umami.is/script.js';
+  script.setAttribute('data-website-id', 'e77e54c0-095f-407b-ba46-f06faa159acd');
+  document.head.appendChild(script);
+})();
